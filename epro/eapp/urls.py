3006b7f709c4e92b80e11b0b8aed2a06303dcbb3
akhilkumar3210 <ak2072719@gmail.com>
2025-03-01 16:09:07 +0530

    mar01

---
+++ epro/eapp/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
